39c637e691fff08c5ab46948dbf64a04c7287eb5
m.mokhtari <m.mokhtari@datxsoft.com>
2021-01-13 09:19:02 +0330

    Resolve crash on web

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import { DeviceEventEmitter, NativeModules, Platform } from 'react-native';
 const { RNSmsRetrieverModule } = NativeModules;
 const EVEN_LISTENER = 'me.furtado.smsretriever:SmsEvent';
 
-const SmsRetrieverModule = (Platform.OS === "ios") ? null : {
+const SmsRetrieverModule = (Platform.OS === "android") ? {
   requestPhoneNumber: RNSmsRetrieverModule.requestPhoneNumber,
   startSmsRetriever: RNSmsRetrieverModule.startSmsRetriever,
   addSmsListener: (callback) => DeviceEventEmitter.addListener(EVEN_LISTENER, callback),
   removeSmsListener: () => DeviceEventEmitter.removeAllListeners(EVEN_LISTENER)
-};
+} : null;
 
 export default SmsRetrieverModule;
